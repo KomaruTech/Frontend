6ca33cf9a515783c3e888865353e056c0c6d3397
Don4ara <don4araa@gmail.com>
2025-07-03 21:02:48 +0500

    refactor(welcome-card): упрощена структура JSX, улучшено форматирование и заменен элемент Image на img

--- src/widgets/WelcomeCard/ui/WelcomeCard.tsx
+++ src/widgets/WelcomeCard/ui/WelcomeCard.tsx
@@ -1,26 +1,26 @@
-// src/widgets/WelcomeCard/ui/WelcomeCard.tsx
 import React from 'react';
-import { useSelector } from 'react-redux';
-import type { RootState } from '@app/store';
-import { Image } from '@heroui/react';
+import {useSelector} from 'react-redux';
+import type {RootState} from '@app/store';
 import WelcomeCardPattern from '@shared/assets/images/WelcomeCardPattern2.svg';
 
 interface WelcomeCardProps {
     userName?: string | null;
 }
 
-export const WelcomeCard: React.FC<WelcomeCardProps> = ({ userName }) => {
+export const WelcomeCard: React.FC<WelcomeCardProps> = ({userName}) => {
     const storedUser = useSelector((state: RootState) => state.auth.user);
     const displayUserName = userName || storedUser?.name || 'Пользователь';
 
     return (
-        <div className="
-      w-full max-w-[705px]
-      bg-white
-      shadow-lg rounded-xl overflow-hidden
-      h-[160px]
-      flex flex-row items-stretch mb-[90px] ml-[60px]
-    ">
+        <div
+            className="
+        w-full
+        bg-white shadow-lg rounded-xl overflow-hidden
+        h-[160px]
+        flex flex-row items-stretch
+      "
+        >
+            {/* Левая текстовая часть */}
             <div className="flex flex-col justify-center flex-grow basis-2/3 p-6">
                 <h2 className="text-3xl sm:text-4xl font-extrabold text-gray-800 mb-1">
                     Привет, {displayUserName}!
@@ -29,15 +29,11 @@ export const WelcomeCard: React.FC<WelcomeCardProps> = ({ userName }) => {
                     Рады видеть тебя снова 👋
                 </p>
             </div>
-            <div className="relative flex-shrink-0 hidden md:block ">
-                <Image
-                    src={WelcomeCardPattern}
-                    alt="Декоративный узор"
-                    className="
-                         right-0 bottom-4 w-[300px] object-cover
-                    "
-                />
-            </div>
+            <img
+                src={WelcomeCardPattern}
+                alt="Декоративный узор"
+                className="h-full w-auto object-contain"
+            />
         </div>
     );
 };
