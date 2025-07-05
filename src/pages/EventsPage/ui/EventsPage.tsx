d535f516fbbf6b3cecc632d64627a51ae96ee0db
Don4ara <don4araa@gmail.com>
2025-07-06 01:44:25 +0500

    refactor(events): упрощена структура сетки для списка прошлых событий

--- src/pages/EventsPage/ui/EventsPage.tsx
+++ src/pages/EventsPage/ui/EventsPage.tsx
@@ -39,7 +39,7 @@ export default function EventsPage() {
                 />
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div>
                 {activeTab === "past" && (
                     <PastEventsList
                         selectedEvent={selectedEvent}
