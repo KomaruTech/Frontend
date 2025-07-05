e8e45df2283659b8ecd0a4d515317fc240486dc2
Don4ara <don4araa@gmail.com>
2025-07-06 01:43:42 +0500

    feat(router): добавлен маршрут для страницы TeamsPage

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -9,6 +9,7 @@ import EditMyProfilePage from "@pages/EditMyProfilePage/ui/EditMyProfilePage.tsx
 import EventsPage from "@pages/EventsPage/ui/EventsPage.tsx"
 import ApplicationsPage from "@pages/ApplicationsPage";
 import FeedbackPage from "@pages/FeedbackPage/ui/FeedbackPage";
+import TeamsPage from "@pages/TeamsPage/ui/TeamsPage";
 
 export const AppRouter: React.FC = () => {
     return (
@@ -22,11 +23,14 @@ export const AppRouter: React.FC = () => {
                     <Route path="/profile/me/edit" element={<EditMyProfilePage />} />
                     <Route path="/events" element={<EventsPage />} />
                     <Route path="/feedback" element={<FeedbackPage />} />
+                    <Route path="/teams" element={<TeamsPage/>} />
                 </Route>
 
                 <Route element={<PrivateRoute allowedRoles={['administrator']} />}>
                     <Route path="/applications" element={<ApplicationsPage />} />
                 </Route>
+
+
 
                 <Route path="*" element={<div>404 - Страница не найдена</div>} />
             </Routes>
